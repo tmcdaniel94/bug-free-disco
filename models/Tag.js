d3083cf9ea71b711eb0ba38b8dce106ecae2634c
tmcdaniel94 <tmcdaniel94@gmail.com>
2024-10-11 17:22:52 -0500

    added content to producttag and tag

--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,14 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    }
   },
   {
     sequelize,
