b06ae4d2a9c6c071b4f0c80a6393fa447f8bf021
tmcdaniel94 <tmcdaniel94@gmail.com>
2024-10-15 21:27:40 -0500

    added tag-routes

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 
 // Products belongToMany Tags (through ProductTag)
+ProductTag.belongsToMany(Tag, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag, {
+  foreignKey: 'product_id',
+})
 
 module.exports = {
   Product,
