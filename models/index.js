fdb765c37b2f9d95b486c10906b4aa4ce76e88ff
tmcdaniel94 <tmcdaniel94@gmail.com>
2024-10-19 15:58:34 -0500

    edited index.js model so seeds run, added to gitignore

--- models/index.js
+++ models/index.js
@@ -13,31 +13,20 @@ Product.belongsTo(Category, {
 Category.hasMany(Product, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE',
-})
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
     // Define the third table needed to store the foreign keys
-    through: {
-      model: ProductTag,
-      unique: false
-    },
-    // Define an alias for when data is retrieved
-    as: 'product_id'
-
-  // foreignKey: 'product_id',
-})
+    through: ProductTag,
+    foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  // Define an alias for when data is retrieved
-  as: 'tag_id'
-  // foreignKey: 'product_id',
-})
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
