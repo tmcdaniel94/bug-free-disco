abac79dc11549f9000189697207b2af0f59cf08a
tmcdaniel94 <tmcdaniel94@gmail.com>
2024-10-22 21:05:19 -0500

    fixed route issue

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,8 +18,8 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = await Tag.findOne(parseInt(req.params.id), {
-      include: [{ model: Product, through: ProductTag, as: 'product_tag' }]
+    const tagData = await Tag.findByPk(parseInt(req.params.id), {
+      include: [{ model: Product, through: ProductTag }]
     });
 
     if (!tagData) {
