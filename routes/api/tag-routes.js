f9cbc6dea456c6bfa68d377656ff11c056773d50
tmcdaniel94 <tmcdaniel94@gmail.com>
2024-10-22 20:50:04 -0500

    edited tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,7 +18,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = await Tag.findByPk(parseInt(req.params.id), {
+    const tagData = await Tag.findOne(parseInt(req.params.id), {
       include: [{ model: Product, through: ProductTag, as: 'product_tag' }]
     });
 
